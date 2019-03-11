7e6ea26873b399550eedb5e2119dd8a09265dda5
alejolaya <alejolaya@gmail.com>
2019-03-10 19:56:06 -0500

    Methoc get users created

--- src/main/java/com/youtube/demo/dao/UserRepository.java
+++ src/main/java/com/youtube/demo/dao/UserRepository.java
@@ -9,6 +9,6 @@ public interface UserRepository extends JpaRepository<User, Long>{
 	@SuppressWarnings("unchecked")
 	User save(User user);
 	
-	
+
 
 }
--- src/main/java/com/youtube/demo/service/UserService.java
+++ src/main/java/com/youtube/demo/service/UserService.java
@@ -1,9 +1,22 @@
 package com.youtube.demo.service;
 
+import java.util.List;
+
 import com.youtube.demo.model.User;
 
 public interface UserService {
-
+/**
+ * guarda los usuarios
+ * @param user
+ * @return
+ */
 	User save(User user);
+/**
+ * recupera la lista de todos los usuarios
+ * @return
+ */
+	List<User> findAll();
+
+	
 
 }
